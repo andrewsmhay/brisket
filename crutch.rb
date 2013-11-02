1187b29bdb7847733f3a224200ae7a0bfa3a280f
Andrew Hay <andrewsmhay@gmail.com>
2013-11-02 06:49:41 -0700

    updated ip ranges

--- crutch.rb
+++ crutch.rb
@@ -2,11 +2,30 @@ require 'nokogiri'
 require 'geoip'
 
 # Special thanks to PentestGeek - http://www.pentestgeek.com/2012/08/23/creds-or-hash-where-the-admin-at/
+opt_sel = ['city','country']
+opt_sel_err = "[-] Usage: ./crutch.rb <results file> <city|country>"
+working_dir = "."
+results_dir = working_dir+"/results/"
+dir_date = Date.today.year.to_s+"/"+Date.today.month.to_s+"/"+Date.today.day.to_s+"/"
+results_dir_date = results_dir + dir_date
+item_dir= []
+commands = []
+ARGV.each {|arg| commands << arg}
 
-if ARGV.size != 1 then
-  puts "[-] Usage: ./crutch.rb <filename.xml>"
-  exit
+puts "[+] Beginning conversion..."
+if ARGV[0] == opt_sel[0]
+  Dir.foreach(results_dir_date) do |item|
+    next if item == '.' or item == '..'
+      item_dir << results_dir_date + item
+      item_xml = item.gsub(/(.ip)/, '.xml')
+    end
+elsif ARGV[0] == opt_sel[1]
+  europe_reg.shuffle.each do |e|
+    system(cmd + " -c " + confpath + e + " --banners --excludefile " + exclude_file)
+  end
+else puts opt_sel_err
 end
+puts "[+] Scan complete."
 
 xml = Nokogiri::XML.parse(open ARGV[0])
 # diplay which ip address and ports are open
