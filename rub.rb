aa18f8aca47bbd5f28b66f2ff8d74d906c9208fd
Andrew Hay <andrewsmhay@gmail.com>
2013-11-11 15:40:01 -0800

    put proper comments in

--- rub.rb
+++ rub.rb
@@ -62,11 +62,11 @@ elsif ARGV[1] == Options.scan_sel[1]
 	else puts Messages.opt_sel_err
 	end
 	puts Messages.scan_complete
-
+=end
 
 elsif ARGV[1] == Options.scan_sel[2]
 	puts Messages.tbd
-
+=begin
 	if ARGV[0] == Options.opt_sel[0]
 		Scanner.zmap_apac
 	elsif ARGV[0] == Options.opt_sel[1]
@@ -84,6 +84,6 @@ elsif ARGV[1] == Options.scan_sel[2]
 	else puts Messages.opt_sel_err
 	end
 	puts Messages.scan_complete
-
+=end
 else puts Messages.rub_opt_sel_err
 end
