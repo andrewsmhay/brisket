cf95ec2edcf3ea9e76d7ae1143ef0961dbc575a1
Andrew Hay <andrewsmhay@gmail.com>
2013-11-10 17:47:17 -0800

    clean up

--- trim.rb
+++ trim.rb
@@ -22,6 +22,12 @@ class Directories
   def self.results_dir_date
     results_dir + dir_date
   end
+  def self.results_out
+    "-oX " + results_dir_date
+  end
+  def self.include_file_cmd
+    " --includefile " + data_dir
+  end
 end
 =begin
 class Masscan
@@ -60,7 +66,7 @@ class Masscan
   end
 end
 =end
-working_dir = "/home/scanner/brisket"
+#working_dir = "/home/scanner/brisket"
 remote_ports = 	"22,23,513,3389,5900"
 app_ports = 	"21,69,53,389,161,1984"
 ms_ports =    "135,139,445"
@@ -72,14 +78,13 @@ special_ports = "6667,9050,1514,20000"
 rate = "2337" #restriction by the service provider is 4000/second
 rate_cmd = "--rate " + rate
 cmd = "/usr/local/sbin/masscan"
-exclude_file = working_dir+"/masscan/data/exclude.conf"
-results_dir = working_dir+"/results/"
-data_dir = working_dir+"/data/"
-include_file_cmd = " --includefile " + data_dir
-conf_dir = working_dir+"/conf/"
-dir_date = Date.today.year.to_s+"/"+Date.today.month.to_s+"/"+Date.today.day.to_s+"/"
-results_dir_date = results_dir + dir_date
-results_out = "-oX " + results_dir_date
+#results_dir = working_dir+"/results/"
+#data_dir = working_dir+"/data/"
+#include_file_cmd = " --includefile " + data_dir
+#conf_dir = working_dir+"/conf/"
+#dir_date = Date.today.year.to_s+"/"+Date.today.month.to_s+"/"+Date.today.day.to_s+"/"
+#results_dir_date = results_dir + dir_date
+#results_out = "-oX " + results_dir_date
 opt_sel = ['remote', 'apps', 'web', 'db','special', 'ms', 'mail', 'all']
 opt_sel_err = "[-] Usage: ./trim.rb <remote|apps|web|db|all> <masscan|nmap|zmap>"
 timenow = Time.new
@@ -90,74 +95,67 @@ commands = []
 ARGV.each {|arg| commands << arg}
 ## Create the latest conf files
 if ARGV[0] == opt_sel[0]  
-=begin	Dir.foreach(data_dir) do |item|
-		next if item == '.' or item == '..'
-  		item_dir = conf_dir + item.gsub(/(.ip)/, '.conf')
-  		item_xml = hostname + "_" + item.gsub(/(.ip)/, '.xml')
-  		system(cmd + " -p" + remote_ports + include_file_cmd + item + " " + rate_cmd + " " + results_out + item_xml + " --echo > " + item_dir)
-	end
-=end
   Dir.foreach(Directories.data_dir) do |item|
     next if item == '.' or item == '..'
       item_dir = Directories.conf_dir + item.gsub(/(.ip)/, '.conf')
       item_xml = hostname + "_" + item.gsub(/(.ip)/, '.xml')
-      system(cmd + " -p" + remote_ports + include_file_cmd + item + " " + rate_cmd + " " + results_out + item_xml + " --echo > " + item_dir)
+      system(cmd + " -p" + remote_ports + Directories.include_file_cmd + item + " " + rate_cmd + " " + Directories.results_out + item_xml + " --echo > " + item_dir)
   end
 	puts conf_txt
 elsif ARGV[0] == opt_sel[1]
-	Dir.foreach(data_dir) do |item|
+	Dir.foreach(Directories.data_dir) do |item|
 		next if item == '.' or item == '..'
-  		item_dir = conf_dir + item.gsub(/(.ip)/, '.conf')
+  		item_dir = Directories.conf_dir + item.gsub(/(.ip)/, '.conf')
   		item_xml = hostname + "_" + item.gsub(/(.ip)/, '.xml')
-  		system(cmd + " -p" + app_ports + include_file_cmd + item + " " + rate_cmd + " " + results_out + item_xml + " --echo > " + item_dir)
+  		system(cmd + " -p" + app_ports + Directories.include_file_cmd + item + " " + rate_cmd + " " + Directories.results_out + item_xml + " --echo > " + item_dir)
 	end	
 	puts conf_txt	
 elsif ARGV[0] == opt_sel[2]
-	Dir.foreach(data_dir) do |item|
+	Dir.foreach(Directories.data_dir) do |item|
 		next if item == '.' or item == '..'
-  		item_dir = conf_dir + item.gsub(/(.ip)/, '.conf')
+  		item_dir = Directories.conf_dir + item.gsub(/(.ip)/, '.conf')
   		item_xml = hostname + "_" + item.gsub(/(.ip)/, '.xml')
-  		system(cmd + " -p" + web_ports + include_file_cmd + item + " " + rate_cmd + " " + results_out + item_xml + " --echo > " + item_dir)
+  		system(cmd + " -p" + web_ports + Directories.include_file_cmd + item + " " + rate_cmd + " " + Directories.results_out + item_xml + " --echo > " + item_dir)
 	end	
 	puts conf_txt	
 elsif ARGV[0] == opt_sel[3]
-	Dir.foreach(data_dir) do |item|
+	Dir.foreach(Directories.data_dir) do |item|
 		next if item == '.' or item == '..'
-  		item_dir = conf_dir + item.gsub(/(.ip)/, '.conf')
+  		item_dir = Directories.conf_dir + item.gsub(/(.ip)/, '.conf')
   		item_xml = hostname + "_" + item.gsub(/(.ip)/, '.xml')
-  		system(cmd + " -p" + db_ports + include_file_cmd + item + " " + rate_cmd + " " + results_out + item_xml + " --echo > " + item_dir)
+  		system(cmd + " -p" + db_ports + Directories.include_file_cmd + item + " " + rate_cmd + " " + Directories.results_out + item_xml + " --echo > " + item_dir)
 	end	
 	puts conf_txt	
 elsif ARGV[0] == opt_sel[4]
-  Dir.foreach(data_dir) do |item|
+  Dir.foreach(Directories.data_dir) do |item|
     next if item == '.' or item == '..'
-      item_dir = conf_dir + item.gsub(/(.ip)/, '.conf')
+      item_dir = Directories.conf_dir + item.gsub(/(.ip)/, '.conf')
       item_xml = hostname + "_" + item.gsub(/(.ip)/, '.xml')
-      system(cmd + " -p" + special_ports + include_file_cmd + item + " " + rate_cmd + " " + results_out + item_xml + " --echo > " + item_dir)
+      system(cmd + " -p" + special_ports + Directories.include_file_cmd + item + " " + rate_cmd + " " + Directories.results_out + item_xml + " --echo > " + item_dir)
   end
   puts conf_txt
 elsif ARGV[0] == opt_sel[5]
-  Dir.foreach(data_dir) do |item|
+  Dir.foreach(Directories.data_dir) do |item|
     next if item == '.' or item == '..'
-      item_dir = conf_dir + item.gsub(/(.ip)/, '.conf')
+      item_dir = Directories.conf_dir + item.gsub(/(.ip)/, '.conf')
       item_xml = hostname + "_" + item.gsub(/(.ip)/, '.xml')
-      system(cmd + " -p" + ms_ports + include_file_cmd + item + " " + rate_cmd + " " + results_out + item_xml + " --echo > " + item_dir)
+      system(cmd + " -p" + ms_ports + Directories.include_file_cmd + item + " " + rate_cmd + " " + Directories.results_out + item_xml + " --echo > " + item_dir)
   end  
   puts conf_txt
 elsif ARGV[0] == opt_sel[6]
-  Dir.foreach(data_dir) do |item|
+  Dir.foreach(Directories.data_dir) do |item|
     next if item == '.' or item == '..'
-      item_dir = conf_dir + item.gsub(/(.ip)/, '.conf')
+      item_dir = Directories.conf_dir + item.gsub(/(.ip)/, '.conf')
       item_xml = hostname + "_" + item.gsub(/(.ip)/, '.xml')
-      system(cmd + " -p" + mail_ports + include_file_cmd + item + " " + rate_cmd + " " + results_out + item_xml + " --echo > " + item_dir)
+      system(cmd + " -p" + mail_ports + Directories.include_file_cmd + item + " " + rate_cmd + " " + Directories.results_out + item_xml + " --echo > " + item_dir)
   end
   puts conf_txt
 elsif ARGV[0] == opt_sel[7]
-	Dir.foreach(data_dir) do |item|
+	Dir.foreach(Directories.data_dir) do |item|
 		next if item == '.' or item == '..'
-  		item_dir = conf_dir + item.gsub(/(.ip)/, '.conf')
+  		item_dir = Directories.conf_dir + item.gsub(/(.ip)/, '.conf')
   		item_xml = hostname + "_" + item.gsub(/(.ip)/, '.xml')
-  		system(cmd + " -p" + remote_ports + "," + app_ports + "," + web_ports + "," + db_ports + "," + special_ports + "," + ms_ports + "," + mail_ports + include_file_cmd + item + " " + rate_cmd + " " + results_out + item_xml + " --echo > " + item_dir)
+  		system(cmd + " -p" + remote_ports + "," + app_ports + "," + web_ports + "," + db_ports + "," + special_ports + "," + ms_ports + "," + mail_ports + Directories.include_file_cmd + item + " " + rate_cmd + " " + Directories.results_out + item_xml + " --echo > " + item_dir)
 	end
 	puts conf_txt
 else puts opt_sel_err
