228c6bc32c32884839e024ff35d371d87dcf7172
Andrew Hay <andrewsmhay@gmail.com>
2013-11-15 19:42:34 -0800

    end

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -97,5 +97,6 @@ class Scanner
       Options.all_reg.shuffle.shuffle.each { |a| system(zmapcmd + " -p " + Ports.remote_ports + zmap_seed + rate_cmd + " -w " + a + " -b " + Directories.blacklist + " -O json " + "-o " + Directories.results_dir_date + Naming.hostname + "_" + Options.prefix[2] + "_" + a.gsub(/.conf/, '') + Options.postfix[3])}
     end
 =end
+    end
   end
 end
