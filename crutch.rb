ad9a32da5fb5a76074103258c2c724dd52d8f230
Andrew Hay <andrewsmhay@gmail.com>
2013-11-17 11:20:07 -0800

    not sure

--- crutch.rb
+++ crutch.rb
@@ -35,9 +35,10 @@ if ARGV[0] == Options.opt_sel_region[0] #apac
       end
   end
   f = File.open(rb_file+".csv", "w")
-  f.each do |g|
-    g.write(inputter)
-  end
+  f.write(inputter)
+  #f.each do |g|
+  #  g.write(inputter)
+  #end
   f.close
 elsif ARGV[0] == Options.opt_sel_region[1] #europe
   Dir.glob(Directories.crutch_results_dir_date+"*"+Options.opt_sel_region[1]+"*.xml") do |rb_file|
