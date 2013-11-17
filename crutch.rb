426a0988c7f04f86a98f97d5fdd64d1adaea8259
Andrew Hay <andrewsmhay@gmail.com>
2013-11-17 11:23:07 -0800

    each

--- crutch.rb
+++ crutch.rb
@@ -34,10 +34,7 @@ if ARGV[0] == Options.opt_sel_region[0] #apac
         end
       end
       f = File.open(rb_file+".csv", "w")
-      #f.write(inputter)
-      rb_file.each do |g|
-        g.write(inputter)
-      end
+      f.write.each(inputter)
       f.close
   end
 elsif ARGV[0] == Options.opt_sel_region[1] #europe
