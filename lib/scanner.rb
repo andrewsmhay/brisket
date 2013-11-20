bbacbd400aedc508d53287102424728898b6e553
Andrew Hay <andrewsmhay@gmail.com>
2013-11-19 20:15:02 -0800

    test

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -25,7 +25,7 @@ class Scanner
     end
     
     def nmap_options
-    	" -sS -Pn -n --max-rate "+rate+" --open --randomize-hosts --send-eth"
+    	" -sS -Pn -n --max-rate "+rate+" --open --randomize-hosts "
     	#" -sS -P0 -n -O --osscan-limit --version-light --max-rate "+rate+" --randomize-hosts --open --reason"
     end
     
@@ -48,11 +48,11 @@ class Scanner
     end
 
     def nmap_eth
-      "venet0\:0"
+      "venet0:0"
     end
 
     def nmap_virt_flags
-      nmap_options + " -e "+nmap_eth+" -S "+nmap_virt_ip+" "+nmap_input_file
+      nmap_options + " -e \""+nmap_eth+"\" -S "+nmap_virt_ip+" "+nmap_input_file
     end
 
     def nmap_virt scans
