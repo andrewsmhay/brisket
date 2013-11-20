bef3c2631ec46f7f33708bc0d319f0a1602370c6
Andrew Hay <andrewsmhay@gmail.com>
2013-11-19 20:46:38 -0800

    fixes

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -38,7 +38,7 @@ class Scanner
     end
     
     def nmap_flags
-    	nmap_virt_options+nmap_input_file
+    	nmap_options+nmap_input_file
     end
     
     def nmap scans
@@ -56,7 +56,7 @@ class Scanner
     end
 
     def nmap_virt_flags
-      nmap_options + " -e \""+nmap_eth+"\" -S "+nmap_virt_ip+nmap_input_file
+      nmap_virt_options + " -e \""+nmap_eth+"\" -S "+nmap_virt_ip+nmap_input_file
     end
 
     def nmap_virt scans
