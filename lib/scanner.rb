41d8584d738de9a643487bf7d1ba6917126b655e
Andrew Hay <andrewsmhay@gmail.com>
2013-11-29 09:39:20 -0800

    added zmap_rate_cmd

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -98,11 +98,15 @@ class Scanner
     def verbosity
       " -q" #quiet
     end
+
+    def zmap_rate_cmd
+      " -r " + rate
+    end
     
     def zmap scans
       scans.shuffle.each do |a|
         Ports.all_ports_ary.each do |b|
-          system(zmapcmd + " -p " + b + zmap_seed + rate_cmd + verbosity + " -w " + Directories.data_dir + a + " -b " + Directories.blacklist + " -O json " + "-o " + Directories.results_dir_date + Naming.hostname + "_" + Options.prefix[2] + "_" + a.gsub(/.ip/, '') + "_port_" + b + Options.postfix[3])
+          system(zmapcmd + " -p " + b + zmap_seed + zmap_rate_cmd + verbosity + " -w " + Directories.data_dir + a + " -b " + Directories.blacklist + " -O json " + "-o " + Directories.results_dir_date + Naming.hostname + "_" + Options.prefix[2] + "_" + a.gsub(/.ip/, '') + "_port_" + b + Options.postfix[3])
         end
       end    
     end
