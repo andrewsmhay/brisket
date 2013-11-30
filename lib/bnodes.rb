0a0c0f521533bb33f6ec7c481bfc35616019d69a
Andrew Hay <andrewsmhay@gmail.com>
2013-11-29 18:18:57 -0800

    added temp node IP

--- lib/bnodes.rb
+++ lib/bnodes.rb
@@ -1,14 +1,10 @@
 class Bnodes
 	class << self
 		def brisket_nodes
-			%w( 173.244.206.13
-				109.68.191.26
-				173.244.206.19
-				109.68.190.145
-				199.175.51.99
-				66.85.144.237
-				46.38.48.66
-				54.204.15.249 )			
+			# insert your own scanner node information
+			%w( 1.2.3.4
+				5.6.7.8
+				9.1.2.3 )
 		end
 	end
 end
