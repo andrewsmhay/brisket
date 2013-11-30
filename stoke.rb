6f927db041d47434e6753032c5ecfcb30edd1b28
Andrew Hay <andrewsmhay@gmail.com>
2013-11-29 18:16:13 -0800

    git update completed

--- stoke.rb
+++ stoke.rb
@@ -1,47 +1,21 @@
 #!/usr/bin/env ruby
-$LOAD_PATH << '/home/scanner/brisket/lib'
+$LOAD_PATH << './lib'
 require 'directories'
 require 'messages'
 require 'naming'
 require 'options'
 require 'bnodes'
-require 'net/ssh'
-require 'minigit'
 
+=begin
+commands = []
+ARGV.each {|arg| commands << arg}
+=end
 
 i=0
-git = MiniGit.new(Directories.working_dir)
-
-def do_tail( session, file )
-  session.open_channel do |channel|
-    channel.on_data do |ch, data|
-      puts "[#{file}] -> #{data}"
-    end
-    channel.exec "tail -10 #{file}"
-  end
-end
-
 puts Messages.update_git
 while i < Bnodes.brisket_nodes.count
-	Net::SSH.start( Bnodes.brisket_nodes[i], 'scanner' ) do |session|
-		session.open_channel do |channel|
-			channel.on_data do |ch, data|
-				puts "[#{file}] -> #{data}"
-			end
-			channel.exec git.pull
-		end
-		#do_tail session, "/var/log/brisket.log"
-	end
+	puts Messages.ssh_to_bnode+Bnodes.brisket_nodes[i]
+	puts Messages.git_pull
+	system("ssh scanner@'#{Bnodes.brisket_nodes[i]}' \"sh -c 'cd /home/scanner/brisket && git pull'\"")
 	i+=1
 end
-
-	Net::SSH.start.new(
-		'#{Bnodes.brisket_nodes[i]}', 'scanner'
-		)
-		do |session|
-
-
-	i += 1
-end
-puts Messages.git_pull
-git.pull
