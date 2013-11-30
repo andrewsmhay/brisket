d1638101eecdafa5604c10e0a9bc2d41a0db2a10
Andrew Hay <andrewsmhay@gmail.com>
2013-11-29 17:42:01 -0800

    testing

--- lib/bnodes.rb
+++ lib/bnodes.rb
@@ -1,21 +1,12 @@
 class Bnodes
-	def do_tail( session, file )
-  			session.open_channel do |channel|
-    			channel.on_data do |ch, data|
-      			puts "[#{file}] -> #{data}"
-    		end
-    		channel.exec "tail -10 #{file}"
-  			end
-		end
 	class << self
 		def brisket_nodes
 			%w( 173.244.206.13
 				109.68.191.26
 				173.244.206.19
-				178.63.114.74
 				109.68.190.145
-				66.85.144.237
 				199.175.51.99
+				66.85.144.237
 				46.38.48.66
 				54.204.15.249 )			
 		end
--- stoke.rb
+++ stoke.rb
@@ -12,9 +12,33 @@ require 'minigit'
 i=0
 git = MiniGit.new(Directories.working_dir)
 
+def do_tail( session, file )
+  session.open_channel do |channel|
+    channel.on_data do |ch, data|
+      puts "[#{file}] -> #{data}"
+    end
+    channel.exec "tail -10 #{file}"
+  end
+end
+
 puts Messages.update_git
 while i < Bnodes.brisket_nodes.count
-	puts Net::SSH.start(Bnodes.brisket_nodes[i], 'scanner') #do |ssh|
+	Net::SSH.start( Bnodes.brisket_nodes[i], 'scanner' ) do |session|
+		session.open_channel do |channel|
+			channel.on_data do |ch, data|
+				puts "[#{file}] -> #{data}"
+			end
+			channel.exec git.pull
+		end
+		#do_tail session, "/var/log/brisket.log"
+	end
+	i+=1
+end
+
+	Net::SSH.start.new(
+		'#{Bnodes.brisket_nodes[i]}', 'scanner'
+		)
+		do |session|
 
 
 	i += 1
