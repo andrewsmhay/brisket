63caf461e12e8b66281de74dedc50a4beef8b104
Andrew Hay <andrewsmhay@gmail.com>
2013-12-01 19:59:25 -0800

    ips

--- lib/bnodes.rb
+++ lib/bnodes.rb
@@ -2,15 +2,9 @@ class Bnodes
 	class << self
 		def brisket_nodes
 			# insert your own scanner node information
-			%w( 173.244.206.13
-109.68.191.26
-66.85.140.77
-173.244.206.19
-109.68.190.145
-66.85.144.237
-199.175.51.99
-46.38.48.66
-54.204.15.249 )
+			%w( 1.2.3.4
+				5.6.7.8
+				9.1.2.3 )
 		end
 	end
 end
