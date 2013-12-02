955d770b3badf79ec90019492d5727750561a162
Andrew Hay <andrewsmhay@gmail.com>
2013-12-01 17:34:25 -0800

    updated ports

--- lib/ports.rb
+++ lib/ports.rb
@@ -1,7 +1,11 @@
 class Ports
   class << self
     def remote_ports_ary
-      ['22','23','513','3389','5900']
+      ['22',      # ssh
+        '23',     # telnet
+        '513',    # rlogin
+        '3389',   # rdp
+        '5900']   # vnc
     end
 
     def remote_ports
@@ -9,7 +13,10 @@ class Ports
     end
 
     def app_ports_ary
-      ['21','69','161','443']
+      ['21',      # ftp
+        '69',     # TFTP
+        '161',    # SNMP
+        '443']    # https
     end
 
     def app_ports
@@ -17,7 +24,12 @@ class Ports
     end
 
     def ms_ports_ary
-      ['135','137','138','139','389','445']
+      ['135',     # DCE/RPC Locator service / DCOM
+        '137',    # NetBIOS NetBIOS Name Service
+        '138',    # NetBIOS NetBIOS Datagram Service
+        '139',    # NetBIOS NetBIOS Session Service
+        '389',    # LDAP
+        '445']    # Microsoft-DS Active Directory, Windows shares
     end
 
     def ms_ports
@@ -25,36 +37,55 @@ class Ports
     end
 
     def mail_ports_ary
-      ['25','110','995','993','465']
+      ['25',      # smtp
+        '110',    # pop3
+        '143',    # imap
+        '995',    # ssl pop3
+        '993',    # ssl imap
+        '465']    # ssl smtp
     end
 
     def mail_ports
-      "25,110,995,993,465"
+      "25,110,143,995,993,465"
     end
 
     def web_ports_ary
-      ['80','8080','8443','8888']
+      ['80',      # http
+        '902',    # SOCKS
+        '3128',   # squid
+        '8080',   # http alternate
+        '8443',   # https alternate
+        '8888']   # http alternate
     end
 
     def web_ports
-      "80,8080,8443,8888"
+      "80,8080,8443,8888,3128,902"
     end
 
     def db_ports_ary
-      ['1433','1521','3306','5432']
+      ['1433',    # MS SQL
+        '1521',   # Oracle
+        '3306',   # MySQL
+        '5432',   # postgresql
+        '6379',   # redis
+        '5984']   # couchdb
     end
 
     def db_ports
-      "1433,1521,3306,5432"
+      "1433,1521,3306,5432,6379,5984"
     end
 
     def special_ports_ary
-      ['6667','9050','1514','1984','3128']
+      ['6667',    # IRC
+        '9050',   # tor
+        '1514',   # tcp syslog
+        '1984',   # rackspace agent
+        '8140',   # puppet
+        '61613']  # puppet orchestration
     end
 
     def special_ports
-      # IRC, tor, tcp syslog, rackspace agent
-      "6667,9050,1514,1984,3128"
+      "6667,9050,1514,1984,8140,61613"
     end
 
     def all_ports_ary
