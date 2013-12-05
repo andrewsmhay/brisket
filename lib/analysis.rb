942a9e2ec7d639200b0933ccd0989b8707bc966c
Andrew Hay <andrewsmhay@gmail.com>
2013-12-05 08:38:02 -0800

    mopping up

---
+++ lib/analysis.rb
@@ -0,0 +1,20 @@
+class Analysis
+	class << self
+		
+		def header
+			"Date,Provider,Region,IP,Latt,Long,Port"
+		end
+
+		def header_plus
+			header+",Banner"
+		end
+
+		def stripip
+			to_s.gsub(/\<address addr\=\"/, '').gsub(/\"\saddrtype\=\"ipv4\"\/\>/, '')
+		end
+
+		def nmap_root
+			root["nmaprun"]
+		end
+	end
+end
--- mop.rb
+++ mop.rb
@@ -5,15 +5,20 @@
 $LOAD_PATH << './lib'
 require 'nokogiri'
 require 'awesome_print'
+require 'analysis'
 
-rb_file_master = Dir.glob("./analysis/*"+ARGV[0]+"*")
+commands = []
+ARGV.each {|arg| commands << arg}
+# 1 type, 2 date
+
+rb_file_master = Dir.glob("./analysis/"+(ARGV[1].gsub(/\-/, '/'))+"*"+ARGV[0]+"*")
 rb_file_master.each do |rb_file|
 	f = File.open(rb_file)
 	doc = Nokogiri::XML(f)
 	root = doc.root
 	puts "[+] "+rb_file.gsub(/\.\/analysis\//, '')
 	if rb_file =~ /masscan/
-		rule_name = root["nmaprun"]
+		Analysis.nmap_rule_name
 		items = root.xpath("host")
 		i = 0
 		until i == items.count
@@ -21,14 +26,14 @@ rb_file_master.each do |rb_file|
 			portarea = items[i].at_xpath("ports")
 			strip_port = /portid\=\"(.+)\"/.match(portarea.to_s)
 			port_only = /(\d{1,5})/.match(strip_port.to_s).to_s
-			strip_ip = iparea.to_s.gsub(/\<address addr\=\"/, '').gsub(/\"\saddrtype\=\"ipv4\"\/\>/, '')
+			strip_ip = iparea.Analysis.stripip
 			puts strip_ip+","+port_only
 			i+=1
 		end
 	elsif rb_file =~ /zmap/
 		reader = File.read(rb_file)
 		p = /"target_port": (\d{1,5}),/.match(reader)[1]
-		puts p
+		
 	elsif rb_file =~ /nmap/
 		rule_name = root["nmaprun"]
 		items = root.xpath("host")
@@ -39,7 +44,7 @@ rb_file_master.each do |rb_file|
 				strip_name = namearea.xpath("hostname")
 			end
 			iparea = items[i].xpath("address")
-			strip_ip = iparea.to_s.gsub(/\<address addr\=\"/, '').gsub(/\"\saddrtype\=\"ipv4\"\/\>/, '')
+			strip_ip = iparea.Analysis.stripip
 			puts strip_ip+","+strip_name.to_s
 			i+=1
 		end
