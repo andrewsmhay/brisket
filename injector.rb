20dc012ad0dcc75ab91227ef2171614a990a609c
Andrew Hay <andrewsmhay@gmail.com>
2013-12-08 10:10:25 -0800

    psh

--- injector.rb
+++ injector.rb
@@ -13,36 +13,62 @@ ec2 = AWS::EC2.new(
 		    :access_key_id => '', # => Enter your own EC2 Access Key ID
 		    :secret_access_key => '') # => Enter your own EC2 Secret Access Key
 
-master_instance = "i-f3367794"
+master_instance = "" # => enter your instance ID
 brisket_mother = ec2.instances[master_instance]
 
 f = File.open(Directories.brisket_log, 'a+')
 
-if brisket_mother.status == "stopped"
-	f.puts Messages.brisket_start
+if brisket_mother.status == ":stopped"
+	puts Messages.brisket_start
+	f.puts Messages.syslog_stamp+Messages.brisket_start
+  	
   	brisket_mother.start
-  	f.puts Messages.brisket_started
-elsif brisket_mother.status == "running"
-	f.puts Messages.brisket_started
-else f.puts "[+] Error, please check Brisket Mothership logs..."
+  	
+  	puts Messages.brisket_started
+  	f.puts Messages.syslog_stamp+Messages.brisket_started
+  	sleep(5.minutes)
+
+  	puts Archive.prep
+	f.puts Messages.syslog_stamp+Archive.prep
+
+	Archive.tar
+
+	puts Archive.copyfile
+	f.puts Messages.syslog_stamp+Archive.copyfile
+
+	Archive.scp
+
+	puts Archive.cleanup
+	f.puts Messages.syslog_stamp+Archive.cleanup
+
+	Archive.cleanup_zip
+
+	puts Archive.cleanup_done
+	f.puts Messages.syslog_stamp+Archive.cleanup_done
+
+	f.close
+elsif brisket_mother.status == ":running"
+	puts Messages.brisket_started
+	f.puts Messages.syslog_stamp+Messages.brisket_started
+
+	puts Archive.prep
+	f.puts Messages.syslog_stamp+Archive.prep
+
+	Archive.tar
+
+	puts Archive.copyfile
+	f.puts Messages.syslog_stamp+Archive.copyfile
+
+	Archive.scp
+
+	puts Archive.cleanup
+	f.puts Messages.syslog_stamp+Archive.cleanup
+
+	Archive.cleanup_zip
+
+	puts Archive.cleanup_done
+	f.puts Messages.syslog_stamp+Archive.cleanup_done
+
+	f.close
+else f.puts Messages.syslog_stamp+Messages.brisket_error
 end
-
-puts Archive.prep
-f.puts Messages.syslog_stamp+Archive.prep
-
-Archive.tar
-
-puts Archive.copyfile
-f.puts Messages.syslog_stamp+Archive.copyfile
-
-Archive.scp
-
-puts Archive.cleanup
-f.puts Messages.syslog_stamp+Archive.cleanup
-
-Archive.cleanup_zip
-
-puts Archive.cleanup_done
-f.puts Messages.syslog_stamp+Archive.cleanup_done
-
-f.close
